8c40c601ee4832f2dec6f8d0960036b6f63e10f0
gurpreet562 <kgurpreet562@gmail.com>
2017-07-12 20:15:10 +0530

    uploaded assignment5.2

---
+++ object.js
@@ -0,0 +1,10 @@
+var person = { 
+    firstName: "Preity", 
+    lastName : "Khanna", 
+    age:46,
+    pob:"kolkata"
+    } 
+document.write("<br>Your FirstName is:&nbsp"  +person.firstName); 
+document.write("<br><br>Your LastName is :&nbsp"  +person.lastName); 
+document.write("<br><br>Your Age is:&nbsp"  +person.age); 
+document.write("<br><br>Your Place of Birth is:&nbsp"  +person.pob); 
